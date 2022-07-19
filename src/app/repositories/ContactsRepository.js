4db8d7390cc165a8d25f15886576a7efae3323e7
Rafael Ribeiro Dias <rafael.dias@evolutionsoft.com.br>
2022-07-19 18:22:39 -0300

    initializing mycontacts backend

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,70 @@
+const { v4 } = require('uuid');
+const db = require('../../database');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Mateus',
+    email: 'mateus@mail.com',
+    phone: '15996576311',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Rafael',
+    email: 'rafael@mail.com',
+    phone: '15996578311',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    return rows;
+  }
+
+  async findById(id) {
+    const [row] = await db.query(`SELECT * FROM contacts WHERE id = $1`, [id]);
+    return row;
+  }
+
+  async findByEmail(email) {
+    const [row] = await db.query(`SELECT * FROM contacts WHERE email = $1`, [email]);
+    return row;
+  }
+
+  async create({
+    name, email, phone, category_id,
+  }) {
+    const [row] = await db.query(`
+    INSERT INTO contacts(name, email, phone, category_id)
+    VALUES($1, $2, $3, $4)
+    RETURNING *
+    `, [name, email, phone, category_id]);
+
+    return row;
+  }
+
+  async update(id, {
+    name, email, phone, category_id,
+  }) {
+    const [row] = await db.query(`
+    UPDATE contacts
+    SET name = $1, email = $2, phone = $3, category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `,[name, email, phone, category_id, id]);
+    return row;
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
